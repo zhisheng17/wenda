4904a1d3e3c13bb55be9dc1e02672b97297a08aa
TZS <1041218129@qq.com>
2016-07-16 15:13:29 +0800

    add 2

---
+++ src/com/nowcoder/Main.java
@@ -0,0 +1,11 @@
+package com.nowcoder;
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello TZS!");
+        System.out.println("hello TZS2!");
+    }
+}
