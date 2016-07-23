16c7a35af5bcf437197799a1e1601ee3adf5b2da
TZS <1041218129@qq.com>
2016-07-23 23:05:39 +0800

    LoginTicket

---
+++ src/main/java/com/nowcoder/model/LoginTicket.java
@@ -0,0 +1,65 @@
+package com.nowcoder.model;
+
+import java.util.Date;
+
+/**
+ * Created by 10412 on 2016/7/23.
+ */
+public class LoginTicket
+{
+    private int id;
+    private int userId;
+    private Date expired;
+    private int status;
+    private String ticket;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public int getUserId()
+    {
+        return userId;
+    }
+
+    public void setUserId(int userId)
+    {
+        this.userId = userId;
+    }
+
+    public Date getExpired()
+    {
+        return expired;
+    }
+
+    public void setExpired(Date expired)
+    {
+        this.expired = expired;
+    }
+
+    public int getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(int status)
+    {
+        this.status = status;
+    }
+
+    public String getTicket()
+    {
+        return ticket;
+    }
+
+    public void setTicket(String ticket)
+    {
+        this.ticket = ticket;
+    }
+}
