d64a67dfafddb2f987d8305e8bb8e20f2791f14f
TZS <1041218129@qq.com>
2016-08-01 21:44:25 +0800

    QuestionService

--- src/main/java/com/nowcoder/service/QuestionService.java
+++ src/main/java/com/nowcoder/service/QuestionService.java
@@ -15,7 +15,16 @@ public class QuestionService {
     @Autowired
     QuestionDAO questionDAO;
 
-    public List<Question> getLatestQuestions(int userId, int offset, int limit) {
+    public int addQuestion(Question question)
+    {
+        //敏感词过滤
+        return questionDAO.addQuestion(question) > 0 ? question.getId() : 0;
+    }
+
+
+
+    public List<Question> getLatestQuestions(int userId, int offset, int limit)
+    {
         return questionDAO.selectLatestQuestions(userId, offset, limit);
     }
 }
