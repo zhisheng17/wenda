7c5877bfda76685a14b92a471ac02449067d037b
TZS <1041218129@qq.com>
2016-08-01 21:54:04 +0800

    QuestionController

---
+++ src/main/java/com/nowcoder/controller/QuestionController.java
@@ -0,0 +1,70 @@
+package com.nowcoder.controller;
+
+import com.nowcoder.model.HostHolder;
+import com.nowcoder.model.Question;
+import com.nowcoder.service.QuestionService;
+import com.nowcoder.util.WendaUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+/**
+ * Created by 10412 on 2016/8/1.
+ */
+public class QuestionController
+{
+    @Autowired
+    QuestionService questionService;
+
+
+    @Autowired
+    HostHolder hostHolder;
+
+
+
+
+    private static final Logger logger = LoggerFactory.getLogger(QuestionController.class);
+
+    @RequestMapping(value = "/question/add", method = {RequestMethod.POST})
+    @ResponseBody
+    public String addQuestion(@RequestParam("title") String title, @RequestParam("content") String content)
+    {
+        try {
+
+            Question question = new Question();
+            question.setContent(content);
+            question.setTitle(title);
+            question.setCreatedDate(new Date());
+            question.setCommentCount(0);
+            if (hostHolder.getUser() == null)
+            {
+                question.setUserId(WendaUtil.ANONYMOUS_USERID);
+            }
+            else
+            {
+                question.setUserId(hostHolder.getUser().getId());
+            }
+
+            if (questionService.addQuestion(question) > 0)
+            {
+                return WendaUtil.getJSONString(0);
+            }
+
+        }catch (Exception e)
+        {
+            logger.error("增加题目失败"+e.getMessage());
+        }
+
+        return WendaUtil.getJSONString(1, "失败");
+
+    }
+
+
+
+}
