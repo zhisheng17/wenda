62d230d606df2d394afff5955ec5f8c4647dda96
TZS <1041218129@qq.com>
2016-08-09 23:13:53 +0800

    Redis实现对评论的踩和赞功能

---
+++ src/main/java/com/nowcoder/service/LikeService.java
@@ -0,0 +1,65 @@
+package com.nowcoder.service;
+
+import com.nowcoder.util.JedisAdapter;
+import com.nowcoder.util.RedisKeyUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by 10412 on 2016/8/4.
+ */
+@Service
+public class LikeService
+{
+    @Autowired
+    JedisAdapter jedisAdapter;
+
+    public long getLikeCount(int entityType, int entityId)
+    {
+        String likeKey = RedisKeyUtil.getLikeKey(entityType, entityId);
+        return jedisAdapter.scard(likeKey);
+    }
+
+    public int getLikeStatus(int userId, int entityType, int entityId)
+    {
+        String likeKey = RedisKeyUtil.getLikeKey(entityType, entityId);
+        if (jedisAdapter.sismenber(likeKey, String.valueOf(userId)))
+        {
+            return 1;
+        }
+        String disLikeKey = RedisKeyUtil.getDisLikeKey(entityType, entityId);
+        return jedisAdapter.sismenber(disLikeKey, String.valueOf(userId)) ? -1 : 0;
+
+    }
+
+
+
+
+    public long like(int userId, int entityType, int entityId)
+    {
+        String likeKey = RedisKeyUtil.getLikeKey(entityType, entityId);
+        jedisAdapter.sadd(likeKey, String.valueOf(userId));
+
+
+        String disLikeKey = RedisKeyUtil.getDisLikeKey(entityType, entityId);
+        jedisAdapter.srem(disLikeKey, String.valueOf(userId));
+
+
+        return jedisAdapter.scard(likeKey);
+    }
+
+
+    public long disLike(int userId, int entityType, int entityId)
+    {
+
+        String disLikeKey = RedisKeyUtil.getDisLikeKey(entityType, entityId);
+        jedisAdapter.sadd(disLikeKey, String.valueOf(userId));
+
+
+        String likeKey = RedisKeyUtil.getLikeKey(entityType, entityId);
+        jedisAdapter.srem(likeKey, String.valueOf(userId));
+
+
+        return jedisAdapter.scard(likeKey);
+    }
+}
--- src/main/java/com/nowcoder/util/JedisAdapter.java
+++ src/main/java/com/nowcoder/util/JedisAdapter.java
@@ -2,15 +2,26 @@ package com.nowcoder.util;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.nowcoder.controller.CommentController;
 import com.nowcoder.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Service;
 import redis.clients.jedis.BinaryClient;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 /**
  * Created by 10412 on 2016/8/8.
  */
-public class JedisAdapter
+@Service
+public class JedisAdapter implements InitializingBean
 {
+    private static final Logger logger = LoggerFactory.getLogger(CommentController.class);
+
+    private JedisPool pool;
+
     public static void print(int index, Object obj)
     {
         System.out.println(String.format("%d, %s", index, obj.toString()));
@@ -169,5 +180,93 @@ public class JedisAdapter
 
 
 
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        pool = new JedisPool("redis://localhost:6379/10");
+    }
+
+
+    public long sadd(String key, String value)
+    {
+        Jedis jedis = null;
+        try {
+            jedis = pool.getResource();
+            return jedis.sadd(key, value);
+        }catch (Exception e)
+        {
+            logger.error("发生异常"+e.getMessage());
+        }
+        finally {
+            if (jedis != null)
+            {
+                jedis.close();
+            }
+        }
+        return 0;
+    }
+
+
+
+    public long srem(String key, String value)
+    {
+        Jedis jedis = null;
+        try {
+            jedis = pool.getResource();
+            return jedis.srem(key, value);
+        }catch (Exception e)
+        {
+            logger.error("发生异常"+e.getMessage());
+        }
+        finally {
+            if (jedis != null)
+            {
+                jedis.close();
+            }
+        }
+        return 0;
+    }
+
+
+
+    public long scard(String key)
+    {
+        Jedis jedis = null;
+        try {
+            jedis = pool.getResource();
+            return jedis.scard(key);
+        }catch (Exception e)
+        {
+            logger.error("发生异常"+e.getMessage());
+        }
+        finally {
+            if (jedis != null)
+            {
+                jedis.close();
+            }
+        }
+        return 0;
+    }
+
+
+
+    public boolean sismenber(String key, String value)
+    {
+        Jedis jedis = null;
+        try {
+            jedis = pool.getResource();
+            return jedis.sismember(key, value);
+        }catch (Exception e)
+        {
+            logger.error("发生异常"+e.getMessage());
+        }
+        finally {
+            if (jedis != null)
+            {
+                jedis.close();
+            }
+        }
+        return false;
     }
 }
