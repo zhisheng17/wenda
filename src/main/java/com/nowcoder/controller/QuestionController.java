749d311f1b5c0a8875a2394fae5c336637b448f2
TZS <1041218129@qq.com>
2016-08-09 23:19:43 +0800

    问题中显示评论的赞人数

--- src/main/java/com/nowcoder/controller/QuestionController.java
+++ src/main/java/com/nowcoder/controller/QuestionController.java
@@ -2,6 +2,7 @@ package com.nowcoder.controller;
 
 import com.nowcoder.model.*;
 import com.nowcoder.service.CommentService;
+import com.nowcoder.service.LikeService;
 import com.nowcoder.service.QuestionService;
 import com.nowcoder.service.UserService;
 import com.nowcoder.util.WendaUtil;
@@ -38,6 +39,9 @@ public class QuestionController
 
     @Autowired
     CommentService commentService;
+
+    @Autowired
+    LikeService likeService;
 
 
     //提问
@@ -95,6 +99,20 @@ public class QuestionController
         {
             ViewObject vo = new ViewObject();
             vo.set("comment", comment);
+
+            if (hostHolder.getUser() == null)
+            {
+                vo.set("liked", 0);
+            }
+            else
+            {
+                vo.set("liked", likeService.getLikeStatus(hostHolder.getUser().getId(), EntityType.ENTITY_COMMENT, comment.getId()));
+            }
+
+            vo.set("likeCount", likeService.getLikeCount(EntityType.ENTITY_COMMENT, comment.getId()));
+
+
+
             vo.set("user", userService.getUser(comment.getUserId()));
             comments.add(vo);
 
