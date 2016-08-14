7fea27dc1ca782b9960cd5069583e8b88d4b6634
TZS <1041218129@qq.com>
2016-08-14 22:47:47 +0800

    add user commentCount  followerCount followeeCount

--- src/main/java/com/nowcoder/controller/HomeController.java
+++ src/main/java/com/nowcoder/controller/HomeController.java
@@ -1,7 +1,8 @@
 package com.nowcoder.controller;
 
-import com.nowcoder.model.Question;
-import com.nowcoder.model.ViewObject;
+import com.nowcoder.model.*;
+import com.nowcoder.service.CommentService;
+import com.nowcoder.service.FollowService;
 import com.nowcoder.service.QuestionService;
 import com.nowcoder.service.UserService;
 import org.slf4j.Logger;
@@ -21,7 +22,8 @@ import java.util.List;
  * Created by 10412 on 2016/7/15.
  */
 @Controller
-public class HomeController {
+public class HomeController
+{
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
@@ -30,12 +32,25 @@ public class HomeController {
     @Autowired
     UserService userService;
 
-    private List<ViewObject> getQuestions(int userId, int offset, int limit) {
+    @Autowired
+    FollowService followService;
+
+    @Autowired
+    CommentService commentService;
+
+    @Autowired
+    HostHolder hostHolder;
+
+
+    private List<ViewObject> getQuestions(int userId, int offset, int limit)
+    {
         List<Question> questionList = questionService.getLatestQuestions(userId, offset, limit);
         List<ViewObject> vos = new ArrayList<>();
-        for (Question question : questionList) {
+        for (Question question : questionList)
+        {
             ViewObject vo = new ViewObject();
             vo.set("question", question);
+            vo.set("followCount", followService.getFollowerCount(EntityType.ENTITY_QUESTION, question.getId()));
             vo.set("user", userService.getUser(question.getUserId()));
             vos.add(vo);
         }
@@ -50,8 +65,27 @@ public class HomeController {
     }
 
     @RequestMapping(path = {"/user/{userId}"}, method = {RequestMethod.GET, RequestMethod.POST})
-    public String userIndex(Model model, @PathVariable("userId") int userId) {
+    public String userIndex(Model model, @PathVariable("userId") int userId)
+    {
         model.addAttribute("vos", getQuestions(userId, 0, 10));
-        return "index";
+
+        User user = userService.getUser(userId);
+        ViewObject vo = new ViewObject();
+        vo.set("user", user);
+        vo.set("commentCount", commentService.getUserCommentCount(userId));
+        vo.set("followerCount", followService.getFollowerCount(EntityType.ENTITY_USER, userId));
+        vo.set("followeeCount", followService.getFolloweeCount(userId, EntityType.ENTITY_USER));
+        if (hostHolder.getUser() != null)
+        {
+            vo.set("followed", followService.isFollower(hostHolder.getUser().getId(), EntityType.ENTITY_USER, userId));
+        }
+        else
+        {
+            vo.set("followed", false);
+        }
+        model.addAttribute("profileUser", vo);
+
+        return "profile";
+
     }
 }
