83bb04ab1ccdadec7ffb9f05082a9424d6056db1
zhisheng17 <1041218129@qq.com>
2016-11-27 18:36:36 +0800

    add solr search engine for all the website (include question.title and question.content)

---
+++ src/main/java/com/nowcoder/controller/SearchController.java
@@ -0,0 +1,74 @@
+package com.nowcoder.controller;
+
+import com.nowcoder.model.EntityType;
+import com.nowcoder.model.Question;
+import com.nowcoder.model.ViewObject;
+import com.nowcoder.service.FollowService;
+import com.nowcoder.service.QuestionService;
+import com.nowcoder.service.SearchService;
+import com.nowcoder.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 10412 on 2016/11/27.
+ */
+@Controller
+public class SearchController
+{
+    private static final Logger logger = LoggerFactory.getLogger(SearchController.class);
+    @Autowired
+    SearchService searchService;
+
+    @Autowired
+    FollowService followService;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    QuestionService questionService;
+
+    @RequestMapping(path = {"/search"}, method = {RequestMethod.GET})
+    public String search(Model model, @RequestParam("q") String keyword,
+                         @RequestParam(value = "offset", defaultValue = "0") int offset,
+                         @RequestParam(value = "count", defaultValue = "10") int count) {
+        try
+        {
+            List<Question> questionList = searchService.searchQuestion(keyword, offset, count, "<em>", "</em>");
+            List<ViewObject> vos = new ArrayList<>();
+            for (Question question : questionList)
+            {
+                Question q = questionService.getById(question.getId());
+                ViewObject vo = new ViewObject();
+                if (question.getContent() != null)
+                {
+                    q.setContent(question.getContent());
+                }
+                if (question.getTitle() != null)
+                {
+                    q.setTitle(question.getTitle());
+                }
+                vo.set("question", q);
+                vo.set("followCount", followService.getFollowerCount(EntityType.ENTITY_QUESTION, question.getId()));
+                vo.set("user", userService.getUser(q.getUserId()));
+                //后期加上评论问题的评论数量
+                vos.add(vo);
+            }
+            model.addAttribute("vos", vos);
+            model.addAttribute("keyword", keyword);
+        } catch (Exception e) {
+            logger.error("搜索评论失败" + e.getMessage());
+        }
+        return "result";
+    }
+}
---
+++ src/main/java/com/nowcoder/service/SearchService.java
@@ -0,0 +1,82 @@
+package com.nowcoder.service;
+
+import com.nowcoder.model.Question;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.client.solrj.response.UpdateResponse;
+import org.apache.solr.common.SolrInputDocument;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by 10412 on 2016/11/27.
+ */
+@Service
+public class SearchService
+{
+
+    //参考官方文档 https://cwiki.apache.org/confluence/display/solr/Using+SolrJ
+
+
+    private static final String SOLR_URL = "http://127.0.0.1:8983/solr/wenda";
+    private HttpSolrClient client = new HttpSolrClient.Builder(SOLR_URL).build();
+
+    private static final String QUESTION_TITLE_FIELD = "question_title";
+    private static final String QUESTION_CONTENT_FIELD = "question_content";
+
+
+    //查找问题函数（关键字，偏移量， 显示数量，高亮）
+    public List<Question> searchQuestion(String keywords, int offset, int count, String hlPre, String hlPos) throws Exception
+    {
+        List<Question> questionList = new ArrayList<>();
+        SolrQuery query = new SolrQuery(keywords);
+        query.setRows(count);
+        query.setStart(offset);
+        query.setHighlight(true);
+        query.setHighlightSimplePre(hlPre);
+        query.setHighlightSimplePost(hlPos);
+        query.set("hl.fl", QUESTION_TITLE_FIELD + "," + QUESTION_CONTENT_FIELD);
+
+        QueryResponse response = client.query(query);
+
+        //解析搜索获得到内容，并进行分类（标题还是内容）
+        for (Map.Entry<String, Map<String, List<String>>> entry : response.getHighlighting().entrySet())
+        {
+            Question q = new Question();
+            q.setId(Integer.parseInt(entry.getKey()));
+            if (entry.getValue().containsKey(QUESTION_CONTENT_FIELD))
+            {
+                List<String> contentList = entry.getValue().get(QUESTION_CONTENT_FIELD);
+                if (contentList.size() > 0)
+                {
+                    q.setContent(contentList.get(0));
+                }
+            }
+            if (entry.getValue().containsKey(QUESTION_TITLE_FIELD))
+            {
+                List<String> titleList = entry.getValue().get(QUESTION_TITLE_FIELD);
+                if (titleList.size() > 0)
+                {
+                    q.setTitle(titleList.get(0));
+                }
+            }
+            questionList.add(q);
+        }
+        return questionList;
+    }
+
+    //问题索引
+    public boolean indexQuestion(int qid, String title, String content) throws Exception
+    {
+        SolrInputDocument doc =  new SolrInputDocument();
+        doc.setField("id", qid);
+        doc.setField(QUESTION_TITLE_FIELD, title);
+        doc.setField(QUESTION_CONTENT_FIELD, content);
+        UpdateResponse response = client.add(doc, 1000);
+        return response != null && response.getStatus() == 0;
+    }
+}
